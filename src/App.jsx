d82b44d534ad8b19c70c1f60a1ef8b1859b821a4
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2025-01-05 14:38:56 +0100

    static info, status section and language list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import React from "react";
+import Header from "./components/Header";
+import Status from "./components/Status";
+import { languages } from "./languages";
+console.log(languages);
 function App() {
-  
+  const languageElements = languages.map((language) => {
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+    return (
+      <span className="chip" key={language.name} style={styles}>
+        {language.name}
+      </span>
+    );
+  });
 
   return (
     <>
-      
-     <main>
-      Game goes here
-     </main>
-  
-     
+      <Header />
+      <Status />
+      <main>
+        <section className="language-chips">
+          {languageElements}
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Status.jsx
@@ -0,0 +1,10 @@
+export default function Status(){
+    return(
+        <>
+        <div className="status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+        </div>
+        </>
+    )
+}
